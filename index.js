8153e156a41c723ece3e711cc3bf988945abd9b2
Shubham Gupta <92355902+Guptashubham789@users.noreply.github.com>
2025-04-17 01:23:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+//import the express module
+const express=require('express');
+const  mongoose  = require('mongoose');
+const authRouter=require('./routes/auth');
+//Define the port number the server will listen on
+const PORT=3000;
+
+//creates an instance of an express application
+//beacouse it gives us the starting point
+
+const app=express();
+//mongodb string
+const DB="mongodb+srv://shubhamtech731159:R6rWZIDht1GduMBv@cluster0.hanqwnh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+//middleware - to register route or to mount routes
+app.use(express.json());
+app.use(authRouter);
+
+mongoose.connect(DB).then(()=>{
+    console.log('mongodb connected');
+});
+
+
+//start the server and listen on the specified port
+
+app.listen(PORT,"0.0.0.0",function(){
+    ///log the number
+    console.log('server is running on port '+PORT)
+});
