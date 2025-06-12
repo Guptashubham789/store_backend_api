3d0bdb8a8f002322183bcc6a5e0eadc12024fe39
Shubham Gupta <92355902+Guptashubham789@users.noreply.github.com>
2025-06-12 16:34:07 +0530

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -46,7 +46,7 @@ productRouter.get('/api/recommend-products',async(req,res)=>{
 productRouter.get('/api/products-by-category/:category',async(req,res)=>{
     try{
         const {category}=req.params;
-      const products=  await Product.find({category});
+      const products=  await Product.find({category,popular:true});
       if(!products || products.length==0){
         return res.status(400).json({msg:"No record found"});
       }else{
